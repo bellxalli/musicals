c1579bb36efbd1aa71ef4e116d304565ee311faf
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-07 14:16:33 -0700

    added rest of content to musical component

--- src/components/Musical.jsx
+++ src/components/Musical.jsx
@@ -1,4 +1,7 @@
 import React from "react"
+import Ratings from "./Ratings"
+import BroadwayHit from "./BroadwayHit"
+
 
 
 
@@ -7,11 +10,22 @@ function Musical(props)
     return (
         <div id={props.props.id}>
             <hr />
-            <h1>{props.props.title}</h1>
-            <ul>
-                <li></li>
-            </ul>
+            <h2>{props.props.title}</h2>
 
+            <Ratings />
+            <BroadwayHit />
+
+            <p>{props.props.title} hit the stage in {props.props.year} and wirtten by 
+                {props.props.composer}. Tickets for this muscial average $250 per person.
+                Critics say it is a {props.props.durationMinutes} minutes of musical entertainment!
+            </p>
+
+            <p>Genres:</p>
+            <ul>
+                {props.props.genres.map((g, index) => (
+                    <li key={index}>{g}</li>
+                ))}
+            </ul>
         </div>
         
 
