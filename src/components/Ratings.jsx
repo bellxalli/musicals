77d586bf608eecfa36c005b44dbd5c7b0f321846
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-10-07 13:50:39 -0700

    setting up Ratings.jsx

---
+++ src/components/Ratings.jsx
@@ -0,0 +1,32 @@
+
+export default function CriticRatings(){
+
+    return(
+
+        <div>
+
+            <p> Critic Ratings </p>
+
+        </div>
+
+    )
+
+
+
+}
+
+export default function AudienceRatings(){
+
+    return(
+
+        <div>
+
+            <p> Audience Ratings </p>
+
+        </div>
+
+    )
+
+
+
+}
